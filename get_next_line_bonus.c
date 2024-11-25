55ca8ec01ab852a65780a8ccd7a44e31c53042c4
Jordi Terradas Cuenca <jterrada@car3s3.42barcelona.com>
2024-11-25 17:32:11 +0100

    refactored

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -2,45 +2,51 @@
 
 #define MAX_FD 1024
 
-static char *read_and_store(int fd, char *chunk) {
-    char buffer[BUFFER_SIZE + 1];
-    char *tmp;
-    ssize_t bytes_read;
+static char	*read_and_store(int fd, char *chunk)
+{
+    char	*buffer;
+    char	*tmp;
+    ssize_t	bytes_read;
 
-    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
+    buffer = malloc(BUFFER_SIZE + 1);
+    if (!buffer)
+        return NULL;
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
+    {
         buffer[bytes_read] = '\0';
-        if (!chunk) {
+        if (!chunk)
             chunk = ft_strdup("");
-        }
         tmp = chunk;
         chunk = ft_strjoin(tmp, buffer);
         free(tmp);
-        if (ft_strchr(chunk, '\n')) {
+        if (ft_strchr(chunk, '\n'))
             break;
-        }
     }
+    free(buffer);
     return chunk;
 }
 
-static char *extract_line(char **chunk) {
-    char *newline_pos;
-    char *line;
-    size_t line_length;
-    size_t remaining_length;
+static char	*extract_line(char **chunk)
+{
+    char	*newline_pos;
+    char	*line;
+    size_t	line_length;
+    size_t	remaining_length;
 
     newline_pos = ft_strchr(*chunk, '\n');
-    if (newline_pos) {
+    if (newline_pos)
+    {
         line_length = newline_pos - *chunk + 1;
         remaining_length = ft_strlen(newline_pos + 1);
-
         line = malloc(line_length + 1);
         if (!line)
-            return (NULL);
+            return NULL;
         ft_strncpy(line, *chunk, line_length);
         line[line_length] = '\0';
-
         ft_memmove(*chunk, newline_pos + 1, remaining_length + 1);
-    } else {
+    }
+    else
+    {
         line = ft_strdup(*chunk);
         free(*chunk);
         *chunk = NULL;
@@ -48,50 +54,57 @@ static char *extract_line(char **chunk) {
     return line;
 }
 
-char *get_next_line(int fd) {
-    static char *chunks[MAX_FD];
-    char *line = NULL;
+char	*get_next_line(int fd)
+{
+    static char	*chunks[MAX_FD];
+    char		*line;
 
-    if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0) {
+    line = NULL;
+    if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0)
         return NULL;
-    }
-
     chunks[fd] = read_and_store(fd, chunks[fd]);
-    if (!chunks[fd] || chunks[fd][0] == '\0') {
+    if (!chunks[fd] || chunks[fd][0] == '\0')
+    {
         free(chunks[fd]);
         chunks[fd] = NULL;
         return NULL;
     }
-
     line = extract_line(&chunks[fd]);
     return line;
 }
 
-// void ft_putstr(char *s) {
-//     while (*s) {
-//         write(1, s, 1);
-//         s++;
-//     }
+// void	ft_putstr(char *s)
+// {
+// 	while (*s)
+// 	{
+// 		write(1, s, 1);
+// 		s++;
+// 	}
 // }
 
-// int main() {
-//     int fd = open("test.txt", O_RDONLY);
-//     char *result;
+// int	main()
+// {
+// 	int		fd;
+// 	char	*result;
 
-//     if (fd < 0) {
-//         perror("Error opening file");
-//         return 1;
-//     }
+// 	fd = open("test.txt", O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		perror("Error opening file");
+// 		return 1;
+// 	}
 
-//     while ((result = get_next_line(fd)) != NULL) {
-//         ft_putstr(result);
-//         free(result);
-//     }
+// 	while ((result = get_next_line(fd)) != NULL)
+// 	{
+// 		ft_putstr(result);
+// 		free(result);
+// 	}
 
-//     if (close(fd) == -1) {
-//         write(2, "Cannot close file.\n", 19);
-//         return 1;
-//     }
+// 	if (close(fd) == -1)
+// 	{
+// 		write(2, "Cannot close file.\n", 19);
+// 		return 1;
+// 	}
 
-//     return 0;
+// 	return 0;
 // }
